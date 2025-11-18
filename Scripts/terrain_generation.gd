7995445b3a70f04d1d1019cdb3d3fd4aa7dddaf2
Marie <marie.cavalla@etu.univ-lyon1.fr>
2025-11-18 17:08:12 +0100

    Résolution des chunks loadés en décalé

--- Scripts/terrain_generation.gd
+++ Scripts/terrain_generation.gd
@@ -5,17 +5,16 @@ extends Node
 @export var noise : FastNoiseLite
 
 var mesh: MeshInstance3D
-var size_depth : int = 100
-var size_width : int = 100
+var size_depth : int = 60
+var size_width : int = 60
 var mesh_resolution : int = 2 #Une valeur haute coûtera plus de temps à générer
 
 var medium_models : Array[PackedScene]
 var small_models : Array[PackedScene]
 var terrain_texture: Texture2D
-var medium_mesh_count = 250
-var small_mesh_count = 350
+var medium_mesh_count = 30
+var small_mesh_count = 30
 
-var distance_before_chunk_loads = 10
 var map_loaded_chunks : Array[Vector2]
 var map_x_offset = -568
 var map_y_offset = 657
@@ -26,22 +25,41 @@ func _ready():
 	noise.offset = Vector3(map_x_offset,map_y_offset,0)
 	player = get_tree().get_nodes_in_group("Player")[0]
 	load_forest_models()
-	load_chunk_if_needed()
+	load_radius_chunks()
 	
 func _process(_delta:float) -> void:
 	load_chunk_if_needed()
 	
+func load_radius_chunks():
+	var temp_chunk_x = floor(player.global_position.x / size_width)
+	var temp_chunk_z = floor(player.global_position.z / size_depth)
+
+	var radius_around_player = [
+		Vector2(temp_chunk_x, temp_chunk_z),
+		Vector2(temp_chunk_x - 1, temp_chunk_z),
+		Vector2(temp_chunk_x, temp_chunk_z - 1),
+		Vector2(temp_chunk_x - 1, temp_chunk_z - 1)
+	]
+
+	for chunk in radius_around_player:
+			var chunk_x = chunk.x * size_width
+			var chunk_z = chunk.y * size_depth
+			map_loaded_chunks.append(chunk)
+			generate(chunk_x, chunk_z)
+
+
 func load_chunk_if_needed():
 	if(!player):
 		return
 	var player_x =  player.global_position.x
 	var player_z =  player.global_position.z
-	var chunk_x:int = (player_x)/(size_width/2-distance_before_chunk_loads)
-	var chunk_z:int = (player_z)/(size_depth/2-distance_before_chunk_loads)
-	
-	var chunk_vector = Vector2(chunk_x,chunk_z)
+	var temp_chunk_x:int = floor(player.global_position.x / size_width)
+	var temp_chunk_z:int = floor(player.global_position.z / size_depth)
+	var chunk_vector = Vector2(temp_chunk_x,temp_chunk_z)
 	if(!map_loaded_chunks.has(chunk_vector)):
 		map_loaded_chunks.push_back(chunk_vector)
+		var chunk_x = temp_chunk_x * size_width
+		var chunk_z = temp_chunk_z * size_depth
 		generate(chunk_x,chunk_z)
 
 func generate(chunk_x:float, chunk_z:float):
@@ -65,7 +83,7 @@ func generate(chunk_x:float, chunk_z:float):
 	#2 - On vient traiter chaque vertex du terrain plat
 	for i in range(data.get_vertex_count()):
 		var vertex = data.get_vertex(i)
-		var y = get_noise_y(vertex.x + chunk_x * size_width,vertex.z + chunk_z * size_depth)
+		var y = get_noise_y(vertex.x + chunk_x,vertex.z + chunk_z)
 		vertex.y = y
 		data.set_vertex(i,vertex)
 		
@@ -80,8 +98,7 @@ func generate(chunk_x:float, chunk_z:float):
 	#4 - On vient créer la mesh à partir de toutes nos données
 	mesh = MeshInstance3D.new()
 	mesh.mesh = surface.commit()
-	mesh.position.x = chunk_x*size_width
-	mesh.position.z = chunk_z*size_depth
+	mesh.position = Vector3(chunk_x + size_width/2, 0, chunk_z + size_depth/2)
 	mesh.create_trimesh_collision() #La collision de notre terrain
 	mesh.cast_shadow = GeometryInstance3D.SHADOW_CASTING_SETTING_ON
 	mesh.add_to_group("NavSource")
@@ -98,14 +115,13 @@ func generate_structures(chunk_x:float, chunk_z:float):
 func generate_from_array(type:String, models_count:int, models_array:Array[PackedScene], chunk_x:float, chunk_z:float) -> void:
 	randomize()
 	for i in range(models_count):
-		var min_x = chunk_x * size_width - size_width / 2
-		var max_x = chunk_x * size_width + size_width / 2
-		var min_z = chunk_z * size_depth - size_depth / 2
-		var max_z = chunk_z * size_depth + size_depth / 2
+		var min_x = chunk_x 
+		var max_x = chunk_x + size_width 
+		var min_z = chunk_z
+		var max_z = chunk_z + size_depth 
 		var x = randf_range(min_x, max_x)
 		var z = randf_range(min_z, max_z)
-		
-		var y = get_noise_y(x, z)
+		var y = get_noise_y(x-size_width/2, z-size_width/2)
 		var model_to_instance = models_array.pick_random()
 		var instance = model_to_instance.instantiate()
 		instance.position = Vector3(x,y,z)
@@ -113,7 +129,7 @@ func generate_from_array(type:String, models_count:int, models_array:Array[Packe
 		if(type == "medium"):
 			instance.scale = Vector3.ONE * randf_range(0.8, 1.2)
 			instance.scale.y = instance.scale.y * 2.5
-			instance.scale.y = instance.scale.x * 1.5
+			instance.scale.x = instance.scale.x * 1.5
 		add_child(instance)
 		var mesh_instance = instance.get_child(0)
 		mesh_instance.add_to_group("Structure")
