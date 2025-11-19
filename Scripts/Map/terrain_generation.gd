e277a21f159d5a5a4761cfc94559974241f6e354
Marie <marie.cavalla@etu.univ-lyon1.fr>
2025-11-19 10:11:15 +0100

    Randomizing de la seed du noise

--- Scripts/Map/terrain_generation.gd
+++ Scripts/Map/terrain_generation.gd
@@ -8,6 +8,8 @@ var mesh: MeshInstance3D
 var size_depth : int = 60
 var size_width : int = 60
 var mesh_resolution : int = 2 #Une valeur haute coûtera plus de temps à générer
+var highest_point = 50
+var frequency = 0.001
 
 var medium_models : Array[PackedScene]
 var small_models : Array[PackedScene]
@@ -19,11 +21,14 @@ var map_loaded_chunks : Array[Vector2]
 var map_x_offset = -568
 var map_y_offset = 657
 
-var highest_point = 50
+var was_player_placed:bool = false
 
 func _ready():
+	noise.seed =  randi()
+	noise.frequency = frequency
 	noise.offset = Vector3(map_x_offset,map_y_offset,0)
 	player = get_tree().get_nodes_in_group("Player")[0]
+	player.position = Vector3(0,get_noise_y(0,0),0)
 	load_forest_models()
 	
 func _process(_delta:float) -> void:
@@ -79,7 +84,6 @@ func generate(chunk_x:float, chunk_z:float):
 		vertex.y = y
 		data.set_vertex(i,vertex)
 		
-	
 	#3 - On applique des directions et des normes sur nos vertices
 	array_plane.clear_surfaces()
 	data.commit_to_surface(array_plane)
@@ -98,7 +102,6 @@ func generate(chunk_x:float, chunk_z:float):
 	
 	#5 - On ajoute les structures sur le terrain
 	generate_structures(chunk_x,chunk_z)
-	
 
 func generate_structures(chunk_x:float, chunk_z:float):
 	generate_from_array("small",small_mesh_count,small_models, chunk_x, chunk_z)
@@ -117,6 +120,9 @@ func generate_from_array(type:String, models_count:int, models_array:Array[Packe
 		var model_to_instance = models_array.pick_random()
 		var instance = model_to_instance.instantiate()
 		instance.position = Vector3(x,y,z)
+		if(!was_player_placed):
+			was_player_placed = true;
+			player.position = Vector3(x+3,y,z)
 		instance.rotation.y = randf() * (PI*2)
 		if(type == "medium"):
 			instance.scale = Vector3.ONE * randf_range(0.8, 1.2)
